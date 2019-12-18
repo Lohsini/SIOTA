f861af36ff9c0d7e282a4cc7fb61cf275108454a
Lohi <lohi3152@gmail.com>
2019-12-18 11:01:36 +0800

    pics js ok

--- javascript/modalimages.js
+++ javascript/modalimages.js
@@ -1,39 +1,94 @@
-// Get the modal
-var modal = document.getElementById("myModal");
-
-// Get the image and insert it inside the modal - use its "alt" text as a caption
-var img = document.getElementById("myImg");
-var modalImg = document.getElementById("img01");
-var captionText = document.getElementById("caption");
+var img = document.getElementById("myImg1_1");
 img.onclick = function(){
-  modal.style.display = "block";
-  modalImg.src = this.src;
-  captionText.innerHTML = this.alt;
+	var modal = document.getElementById("myModal1_1");
+	var modalImg = document.getElementById("img1_1");
+	var captionText = document.getElementById("caption1_1");
+	modal.style.display = "block";
+	modalImg.src = this.src;
+	captionText.innerHTML = this.alt;
 };
-
 var div = document.getElementsByClassName("closex")[0];
-
 div.onclick = function() {
-  modal.style.display = "none";
+	var modal = document.getElementById("myModal1_1");
+  	modal.style.display = "none";
 };
 
-// ------------------------------------------
 
-// Get the modal
-var modal = document.getElementById("myModal1_2");
-
-// Get the image and insert it inside the modal - use its "alt" text as a caption
 var img = document.getElementById("myImg1_2");
-var modalImg = document.getElementById("img1_2");
-var captionText = document.getElementById("caption1_2");
 img.onclick = function(){
-  modal.style.display = "block";
-  modalImg.src = this.src;
-  captionText.innerHTML = this.alt;
+	var modal = document.getElementById("myModal1_2");
+	var modalImg = document.getElementById("img1_2");
+	var captionText = document.getElementById("caption1_2");
+	modal.style.display = "block";
+	modalImg.src = this.src;
+	captionText.innerHTML = this.alt;
+};
+var div = document.getElementsByClassName("closex")[1];
+div.onclick = function() {
+	var modal = document.getElementById("myModal1_2");
+  	modal.style.display = "none";
 };
 
-var div = document.getElementsByClassName("closex")[0];
 
+var img = document.getElementById("myImg1_3");
+img.onclick = function(){
+	var modal = document.getElementById("myModal1_3");
+	var modalImg = document.getElementById("img1_3");
+	var captionText = document.getElementById("caption1_3");
+	modal.style.display = "block";
+	modalImg.src = this.src;
+	captionText.innerHTML = this.alt;
+};
+var div = document.getElementsByClassName("closex")[2];
 div.onclick = function() {
-  modal.style.display = "none";
+	var modal = document.getElementById("myModal1_3");
+  	modal.style.display = "none";
+};
+
+
+var img = document.getElementById("myImg3_1");
+img.onclick = function(){
+	var modal = document.getElementById("myModal3_1");
+	var modalImg = document.getElementById("img3_1");
+	var captionText = document.getElementById("caption3_1");
+	modal.style.display = "block";
+	modalImg.src = this.src;
+	captionText.innerHTML = this.alt;
+};
+var div = document.getElementsByClassName("closex")[3];
+div.onclick = function() {
+	var modal = document.getElementById("myModal3_1");
+  	modal.style.display = "none";
+};
+
+
+var img = document.getElementById("myImg3_2");
+img.onclick = function(){
+	var modal = document.getElementById("myModal3_2");
+	var modalImg = document.getElementById("img3_2");
+	var captionText = document.getElementById("caption3_2");
+	modal.style.display = "block";
+	modalImg.src = this.src;
+	captionText.innerHTML = this.alt;
+};
+var div = document.getElementsByClassName("closex")[4];
+div.onclick = function() {
+	var modal = document.getElementById("myModal3_2");
+  	modal.style.display = "none";
+};
+
+
+var img = document.getElementById("myImg3_3");
+img.onclick = function(){
+	var modal = document.getElementById("myModal3_3");
+	var modalImg = document.getElementById("img3_3");
+	var captionText = document.getElementById("caption3_3");
+	modal.style.display = "block";
+	modalImg.src = this.src;
+	captionText.innerHTML = this.alt;
+};
+var div = document.getElementsByClassName("closex")[5];
+div.onclick = function() {
+	var modal = document.getElementById("myModal3_3");
+  	modal.style.display = "none";
 };
