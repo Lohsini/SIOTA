c580837e53396e25c864ee307ec1d10ee29194e6
Lohi <lohi3152@gmail.com>
2020-02-20 14:46:46 +0800

    new news

--- javascript/news.js
+++ javascript/news.js
@@ -1,53 +1,50 @@
-var href_1 = "https://www.digitimes.com.tw/iot/article.asp?cat=158&cat1=20&cat2=&id=0000576035_YEW194PQ2TJEYF6CHS4YR";
-var title_1 = "【物聯網報報-DIGITIMES】區塊鏈金融卡上路！可支援150種數位與法定貨幣";
-var day_1 = "2020-01-13";
+var links = [
+  {
+    href:"https://www.digitimes.com.tw/iot/article.asp?cat=158&cat1=20&cat2=&id=0000576035_YEW194PQ2TJEYF6CHS4YR",
+    title:"【物聯網報報-DIGITIMES】區塊鏈金融卡上路！可支援150種數位與法定貨幣",
+    day:"2020-01-13"
+  },
+  {
+    href:"https://www.digitimes.com.tw/iot/article.asp?cat=158&cat1=20&cat2=&id=0000575564_HYS6IUTU5S0KYJ5N5WVMT",
+    title:"【物聯網報報-DIGITIMES】5家西班牙銀行聯合運用智慧合約進行跨行支付作業測試",
+    day:"2020-01-10"
+  },
+  {
+    href:"https://technews.tw/2020/01/04/sky-shepherds-the-farmers-using-drones-to-watch-their-flocks-by-flight/",
+    title:"【物聯網報報-科技新報】無人機牧羊、AI 養豬，可能就是農業的未來",
+    day:"2020-01-08"
+  },
+  {
+    href:"https://www.digitimes.com.tw/iot/article.asp?cat=158&cat1=20&cat2=&id=0000575940_EN48IKJI5D0EIULK4K8XU",
+    title:"【物聯網報報-DIGITIMES】AI、區塊鏈和邊緣運算在工業4.0中的關鍵作用",
+    day:"2020-01-06"
+  },
+  {
+    href:"https://city.gvm.com.tw/article.html?id=70544&utm_source=edm&utm_medium=email&utm_campaign=daily&utm_content=city_post",
+    title:"【物聯網報報-城市學】5G釋照標破天價！前中華電信董事長呂學錦告訴你為什麼",
+    day:"2019-12-31"
+  }
+];
 
-var href_2 = "https://www.digitimes.com.tw/iot/article.asp?cat=158&cat1=20&cat2=&id=0000575564_HYS6IUTU5S0KYJ5N5WVMT";
-var title_2 = "【物聯網報報-DIGITIMES】5家西班牙銀行聯合運用智慧合約進行跨行支付作業測試";
-var day_2 = "2020-01-10";
+var newsSection = document.getElementById("newsSection");
 
-var href_3 = "https://technews.tw/2020/01/04/sky-shepherds-the-farmers-using-drones-to-watch-their-flocks-by-flight/";
-var title_3 = "【物聯網報報-科技新報】無人機牧羊、AI 養豬，可能就是農業的未來";
-var day_3 = "2020-01-08";
+for (var i = 0; i < links.length; i++) {
+  var newsWrapper = document.createElement("div");
 
-var href_4 = "https://www.digitimes.com.tw/iot/article.asp?cat=158&cat1=20&cat2=&id=0000575940_EN48IKJI5D0EIULK4K8XU";
-var title_4 = "【物聯網報報-DIGITIMES】AI、區塊鏈和邊緣運算在工業4.0中的關鍵作用";
-var day_4 = "2020-01-06";
+  var newsicon = document.createElement("i");
+  newsicon.setAttribute("class","fas fa-dove");
+  newsWrapper.appendChild(newsicon);
 
-var href_5 = "https://city.gvm.com.tw/article.html?id=70544&utm_source=edm&utm_medium=email&utm_campaign=daily&utm_content=city_post";
-var title_5 = "【物聯網報報-城市學】5G釋照標破天價！前中華電信董事長呂學錦告訴你為什麼";
-var day_5 = "2019-12-31";
+  var newstitle = document.createElement("a");
+  newstitle.setAttribute("href", links[i].href);
+  newstitle.innerHTML = links[i].title;
+  newsWrapper.appendChild(newstitle);
 
 
+  var newsdate = document.createElement("p");
+  newsdate.innerHTML = links[i].day;
+  newsWrapper.appendChild(newsdate);
 
 
-var news_1 = document.getElementById("news_1");
-news_1.href = href_1;
-news_1.innerHTML = title_1;
-var date_1 = document.getElementById("date_1");
-date_1.textContent = day_1;
-
-
-var news_2 = document.getElementById("news_2");
-news_2.href = href_2;
-news_2.innerHTML = title_2;
-var date_2 = document.getElementById("date_2");
-date_2.textContent = day_2;
-
-var news_3 = document.getElementById("news_3");
-news_3.href = href_3;
-news_3.innerHTML = title_3;
-var date_3 = document.getElementById("date_3");
-date_3.textContent = day_3;
-
-var news_4 = document.getElementById("news_4");
-news_4.href = href_4;
-news_4.innerHTML = title_4;
-var date_4 = document.getElementById("date_4");
-date_4.textContent = day_4;
-
-var news_5 = document.getElementById("news_5");
-news_5.href = href_5;
-news_5.innerHTML = title_5;
-var date_5 = document.getElementById("date_5");
-date_5.textContent = day_5;
+  newsSection.appendChild(newsWrapper);
+}
