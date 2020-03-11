51937f7185aaf98de2b508c3704c819ef626c451
Lohi <lohi3152@gmail.com>
2020-03-11 10:45:57 +0800

    firstpart css class ok

---
+++ javascript/modalimages1.js
@@ -0,0 +1,94 @@
+var img = document.getElementById("card_img_status_1");
+img.onclick = function(){
+	var modal = document.getElementById("modal_card_status_1");
+	var modalImg = document.getElementById("modal_img_status_1");
+	var captionText = document.getElementById("modal_content_status_1");
+	modal.style.display = "block";
+	modalImg.src = this.src;
+	captionText.innerHTML = this.alt;
+};
+var div = document.getElementsByClassName("close_card_btn")[0];
+div.onclick = function() {
+	var modal = document.getElementById("modal_card_status_1");
+  	modal.style.display = "none";
+};
+
+
+var img = document.getElementById("card_img_status_2");
+img.onclick = function(){
+	var modal = document.getElementById("modal_card_status_2");
+	var modalImg = document.getElementById("modal_img_status_2");
+	var captionText = document.getElementById("modal_content_status_2");
+	modal.style.display = "block";
+	modalImg.src = this.src;
+	captionText.innerHTML = this.alt;
+};
+var div = document.getElementsByClassName("close_card_btn")[1];
+div.onclick = function() {
+	var modal = document.getElementById("modal_card_status_2");
+  	modal.style.display = "none";
+};
+
+
+var img = document.getElementById("card_img_status_3");
+img.onclick = function(){
+	var modal = document.getElementById("modal_card_status_3");
+	var modalImg = document.getElementById("modal_img_status_3");
+	var captionText = document.getElementById("modal_content_status_3");
+	modal.style.display = "block";
+	modalImg.src = this.src;
+	captionText.innerHTML = this.alt;
+};
+var div = document.getElementsByClassName("close_card_btn")[2];
+div.onclick = function() {
+	var modal = document.getElementById("modal_card_status_3");
+  	modal.style.display = "none";
+};
+
+
+var img = document.getElementById("card_img_performance_1");
+img.onclick = function(){
+	var modal = document.getElementById("modal_card_performance_1");
+	var modalImg = document.getElementById("modal_img_performance_1");
+	var captionText = document.getElementById("modal_content_performance_1");
+	modal.style.display = "block";
+	modalImg.src = this.src;
+	captionText.innerHTML = this.alt;
+};
+var div = document.getElementsByClassName("close_card_btn")[3];
+div.onclick = function() {
+	var modal = document.getElementById("modal_card_performance_1");
+  	modal.style.display = "none";
+};
+
+
+var img = document.getElementById("card_img_performance_2");
+img.onclick = function(){
+	var modal = document.getElementById("modal_card_performance_2");
+	var modalImg = document.getElementById("modal_img_performance_2");
+	var captionText = document.getElementById("modal_content_performance_2");
+	modal.style.display = "block";
+	modalImg.src = this.src;
+	captionText.innerHTML = this.alt;
+};
+var div = document.getElementsByClassName("close_card_btn")[4];
+div.onclick = function() {
+	var modal = document.getElementById("modal_card_performance_2");
+  	modal.style.display = "none";
+};
+
+
+var img = document.getElementById("card_img_performance_3");
+img.onclick = function(){
+	var modal = document.getElementById("modal_card_performance_3");
+	var modalImg = document.getElementById("modal_img_performance_3");
+	var captionText = document.getElementById("modal_content_performance_3");
+	modal.style.display = "block";
+	modalImg.src = this.src;
+	captionText.innerHTML = this.alt;
+};
+var div = document.getElementsByClassName("close_card_btn")[5];
+div.onclick = function() {
+	var modal = document.getElementById("modal_card_performance_3");
+  	modal.style.display = "none";
+};
