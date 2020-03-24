bd762563672f52f4a6d97801646746c1ea7e0e17
Lohi <lohi3152@gmail.com>
2020-03-24 11:55:48 +0800

    7

--- news/news.php
+++ news/news.php
@@ -1,7 +1,12 @@
-
-
-
 <?php
+
+// $file_path = "news_data.txt";
+// if(file_exists($file_path)){
+// $fp = fopen($file_path,"r");
+// $str = fread($fp,filesize($file_path));
+// echo $str = str_replace("\r\n","<br />",$str);
+// }
+
 
 $filename = "news_data.txt";
 
