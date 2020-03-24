5f4a0b82278136c39f8775619247b0685f3770bc
Lohi <lohi3152@gmail.com>
2020-03-24 11:53:13 +0800

    5

--- news/news.php
+++ news/news.php
@@ -1,8 +1,33 @@
+<!-- <?php
+// $file_path = "news_data.txt";
+// if(file_exists($file_path)){
+// $fp = fopen($file_path,"r");
+// $str = fread($fp,filesize($file_path));
+// echo $str = str_replace("\r\n","<br />",$str);
+// }
+?> -->
+
+
 <?php
-$file_path = "news_data.txt";
-if(file_exists($file_path)){
-$fp = fopen($file_path,"r");
-$str = fread($fp,filesize($file_path));//指定讀取大小，這裡把整個檔案內容讀取出來
-echo $str = str_replace("\r\n","<br />",$str);
+
+$filename = "news_data.txt";
+
+if (!file_exists($filename)) {
+  exec("python3 news.py");
 }
+
+$countDown = 10;
+while(!file_exists($filename) && $countDown >= 0) {
+  sleep(3);
+  $countDown -= 1;
+}
+
+if ($countDown < 0) {
+  echo "[error]";
+  exit(1);
+}
+
+$myfile = fopen($filename, "r") or die("Unable to open file!");
+echo fread($myfile,filesize($filename));
+fclose($myfile);
 ?>
