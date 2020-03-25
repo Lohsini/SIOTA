8dacca2126bed1848bf79b53d8f32efb4f5abccc
Lohsini <lohi3152@gmail.com>
2020-03-25 16:29:28 +0800

    24 hours renew

--- news/news.php
+++ news/news.php
@@ -1,31 +1,39 @@
 <?php
+$now = time();
 
-// $file_path = "news_data.txt";
-// if(file_exists($file_path)){
-// $fp = fopen($file_path,"r");
-// $str = fread($fp,filesize($file_path));
-// echo $str = str_replace("\r\n","<br />",$str);
-// }
+$file = fopen("timestamp.txt","r+");
+$last = fread($file,"100");
+echo "上一次更新時間：".$last;
+fclose($file);
 
+if ($now - $last > 10) {
+  $file = fopen("timestamp.txt","w+");
+  fwrite($file,$now);
+  echo "目前更新時間：".$now;
+  fclose($file);
 
-$filename = "news_data.txt";
+  $filename = "news_data.txt";
 
-if (!file_exists($filename)) {
-  exec("python3 news.py");
+  if (!file_exists($filename)) {
+    exec("python3 news.py");
+  }
+
+  $countDown = 10;
+  while(!file_exists($filename) && $countDown >= 0) {
+    sleep(3);
+    $countDown -= 1;
+  }
+
+  if ($countDown < 0) {
+    echo "[error]";
+    exit(1);
+  }
+
+  $myfile = fopen($filename, "r") or die("Unable to open file!");
+  echo fread($myfile,filesize($filename));
+  fclose($myfile);
 }
-
-$countDown = 10;
-while(!file_exists($filename) && $countDown >= 0) {
-  sleep(3);
-  $countDown -= 1;
+else {
+  echo "目前狀態：尚未更新";
 }
-
-if ($countDown < 0) {
-  echo "[error]";
-  exit(1);
-}
-
-$myfile = fopen($filename, "r") or die("Unable to open file!");
-echo fread($myfile,filesize($filename));
-fclose($myfile);
 ?>
