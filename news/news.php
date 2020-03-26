1e2ec99dd7cca5b7dd41859eff87d1dd89b978ec
Lohsini <lohi3152@gmail.com>
2020-03-26 17:00:52 +0800

    test

--- news/news.php
+++ news/news.php
@@ -12,32 +12,36 @@ fclose($timestamp);
 echo "這是if之外//";
 
 if ($now - $last > 60) {
-  $timestamp = fopen($timestampfile,"w+");
-  fwrite($timestamp,$now);
-  fclose($timestamp);
+  echo "這是if裡面//";
 
-  echo "這是if裡面且表示時間標已經換好了//";
-  // 如果時間超過且檔案不在，就執行原有的
   if (!file_exists($datafile)) {
-    echo "如果時間超過且檔案不在//";
+    echo "如果時間超過且檔案不在，就執行原有的//";
     $data = fopen($datafile, "r") or die("Unable to open file!");
     echo fread($data,filesize($datafile));
     fclose($data);
   }
   else{
-    // 1.刪除data
     echo "如果時間超過且檔案在就把他刪除//";
+    // 1.刪除data
     unlink($datafile);
     echo "已刪除完成//";
+
     // 2.啟用python-會建立新的data
     exec("python3 news.py");
     echo "已啟用python//";
-    sleep(3);
+
     // 3.讀取新data資料
+    sleep(3);
     echo "睡了3秒有抓到嗎？//";
+
     $data = fopen($datafile, "r") or die("Unable to open file!可惡//");
     echo fread($data,filesize($datafile));
     fclose($data);
+
+    $timestamp = fopen($timestampfile,"w+");
+    fwrite($timestamp,$now);
+    fclose($timestamp);
+    echo "新檔案ok時間標已經換好了//";
   }
 }
 // else {
