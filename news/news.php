eeb7afdffeedbe93688154e7b548932d311ea4cf
Lohsini <lohi3152@gmail.com>
2020-03-26 17:19:42 +0800

    test no data

--- news/news.php
+++ news/news.php
@@ -19,6 +19,10 @@ if ($now - $last > 60) {
   fclose($timestamp);
   echo "新檔案ok時間標已經換好了//";
 
+  $data = fopen($datafile, "r") or die("Unable to open file!");
+  echo fread($data,filesize($datafile));
+  fclose($data);
+
 //   if (!file_exists($datafile)) {
 //     echo "如果時間超過且檔案不在，就執行原有的//";
 //     $data = fopen($datafile, "r") or die("Unable to open file!");
