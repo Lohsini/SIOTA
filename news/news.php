26e1a8160bc4f2c25b9a0e95bbf11d1b0e39ad0f
Lohsini <lohi3152@gmail.com>
2020-03-26 16:21:37 +0800

    test

--- news/news.php
+++ news/news.php
@@ -17,8 +17,16 @@ if ($now - $last > 60) {
   fclose($timestamp);
 
   echo "這是if裡面且表示時間標已經換好了";
-  // 1.刪除data
-  if(file_exists($datafile)){
+  // 如果時間超過且檔案不在，就執行原有的
+  if (!file_exists($datafile)) {
+    echo "如果時間超過且檔案不在";
+    $data = fopen($datafile, "r") or die("Unable to open file!");
+    echo fread($data,filesize($datafile));
+    fclose($data);
+  }
+  else{
+    // 1.刪除data
+    echo "如果時間超過且檔案在就把他刪除";
     unlink($datafile);
     echo "已刪除完成";
     // 2.啟用python-會建立新的data
@@ -32,7 +40,7 @@ if ($now - $last > 60) {
   }
 }
 else {
-  // 讀取一般data資料
+  // 如果時間沒超過，就執行原有的
   echo "這是else裡面且後面緊接著data";
   $data = fopen($datafile, "r") or die("Unable to open file!");
   echo fread($data,filesize($datafile));
