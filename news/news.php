5b91c0187e685d53de5d19e6586f2a538a850c42
Lohsini <lohi3152@gmail.com>
2020-03-26 16:09:50 +0800

    delete data and call python new

--- news/news.php
+++ news/news.php
@@ -1,44 +1,53 @@
 <?php
 header("Access-Control-Allow-Origin: *");
 
+$datafile = "news_data.txt";
+$timestampfile = "timestamp.txt";
 $now = time();
 
-$file = fopen("timestamp.txt","r+");
-$last = fread($file,"100");
-fclose($file);
-// echo "now".$now;
-// echo "last".$last;
-echo "小於60秒可以看到這行程式";
+$timestamp = fopen($timestampfile,"r+");
+$last = fread($timestamp,"100");
+fclose($timestamp);
+
+
 if ($now - $last > 60) {
-  $file = fopen("timestamp.txt","w+");
-  fwrite($file,$now);
-  fclose($file);
-  exec("python3 news.py");
-  echo "大於60秒才可以看到這行程式";
+  $timestamp = fopen($timestampfile,"w+");
+  fwrite($timestamp,$now);
+  fclose($timestamp);
+
+  // 1.刪除data
+  if(file_exists($datafile)){
+    unlink($datafile);
+
+    // 2.啟用python-會建立新的data
+    exec("python3 news.py");
+
+    // 3.讀取新data資料
+    $data = fopen($datafile, "r") or die("Unable to open file!");
+    echo fread($data,filesize($datafile));
+    fclose($data);
+  }
+}
+else {
+  // 讀取一般data資料
+  $data = fopen($datafile, "r") or die("Unable to open file!");
+  echo fread($data,filesize($datafile));
+  fclose($data);
 }
 
-  $filename = "news_data.txt";
+// if (!file_exists($datafile)) {
 
-  // if (!file_exists($filename)) {
-  //   exec("python3 news.py");
-  // }
+//   exec("python3 news.py");
+// }
 
-  // $countDown = 10;
-  // while(!file_exists($filename) && $countDown >= 0) {
-  //   sleep(3);
-  //   $countDown -= 1;
-  // }
-  //
-  // if ($countDown < 0) {
-  //   echo "[error]";
-  //   exit(1);
-  // }
+// $countDown = 10;
+// while(!file_exists($datafile) && $countDown >= 0) {
+//   sleep(3);
+//   $countDown -= 1;
+// }
 
-$myfile = fopen($filename, "r") or die("Unable to open file!");
-echo fread($myfile,filesize($filename));
-fclose($myfile);
-
-// else {
-//  echo "目前狀態：尚未更新";
+// if ($countDown < 0) {
+//   echo "[error]";
+//   exit(1);
 // }
 ?>
