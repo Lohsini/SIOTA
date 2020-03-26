84788ad737b78ff468c2e827ce145a174b812a70
Lohsini <lohi3152@gmail.com>
2020-03-26 14:40:12 +0800

    change keywords

--- news/news.php
+++ news/news.php
@@ -15,7 +15,7 @@ if ($now - $last > 10) {
   fclose($file);
   exec("python3 news.py");
   echo "已經過了執行python那行程式";
-  // $filename = "news_data.txt";
+  $filename = "news_data.txt";
 
   // if (!file_exists($filename)) {
   //   exec("python3 news.py");
@@ -32,9 +32,9 @@ if ($now - $last > 10) {
   //   exit(1);
   // }
 
-  // $myfile = fopen($filename, "r") or die("Unable to open file!");
-  // echo fread($myfile,filesize($filename));
-  // fclose($myfile);
+  $myfile = fopen($filename, "r") or die("Unable to open file!");
+  echo fread($myfile,filesize($filename));
+  fclose($myfile);
 }
 // else {
 //  echo "目前狀態：尚未更新";
