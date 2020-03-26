6b388d4c08ebe09510d3bcaed717b975be1ff826
Lohsini <lohi3152@gmail.com>
2020-03-26 16:15:19 +0800

    test

--- news/news.php
+++ news/news.php
@@ -9,20 +9,23 @@ $timestamp = fopen($timestampfile,"r+");
 $last = fread($timestamp,"100");
 fclose($timestamp);
 
+echo "這是if之外";
 
 if ($now - $last > 60) {
   $timestamp = fopen($timestampfile,"w+");
   fwrite($timestamp,$now);
   fclose($timestamp);
 
+  echo "這是if裡面且表示時間標已經換好了";
   // 1.刪除data
   if(file_exists($datafile)){
     unlink($datafile);
-
+    echo "已刪除完成";
     // 2.啟用python-會建立新的data
     exec("python3 news.py");
-
+    echo "已啟用python";
     // 3.讀取新data資料
+    echo "這是在if裡面抓到新的data後面就是新的";
     $data = fopen($datafile, "r") or die("Unable to open file!");
     echo fread($data,filesize($datafile));
     fclose($data);
@@ -30,6 +33,7 @@ if ($now - $last > 60) {
 }
 else {
   // 讀取一般data資料
+  echo "這是else裡面且後面緊接著data";
   $data = fopen($datafile, "r") or die("Unable to open file!");
   echo fread($data,filesize($datafile));
   fclose($data);
