2cff8d57c05dec8f1d8554b515ed54e95ebc746c
Lohsini <lohi3152@gmail.com>
2020-03-26 14:52:35 +0800

    60s test

--- news/news.php
+++ news/news.php
@@ -8,13 +8,13 @@ $last = fread($file,"100");
 fclose($file);
 // echo "now".$now;
 // echo "last".$last;
-
+echo "小於60秒可以看到這行程式";
 if ($now - $last > 60) {
   $file = fopen("timestamp.txt","w+");
   fwrite($file,$now);
   fclose($file);
   exec("python3 news.py");
-  echo "那於60秒才可以看到這行程式";
+  echo "大於60秒才可以看到這行程式";
   $filename = "news_data.txt";
 
   // if (!file_exists($filename)) {
