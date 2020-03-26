ea1afbf595cf0914ca11595e12d1f71612fdc1b3
Lohsini <lohi3152@gmail.com>
2020-03-26 11:46:06 +0800

    test

--- news/news.php
+++ news/news.php
@@ -13,23 +13,24 @@ if ($now - $last > 10) {
   $file = fopen("timestamp.txt","w+");
   fwrite($file,$now);
   fclose($file);
+  exec("python3 news.py");
 
-  $filename = "news_data.txt";
-
-  if (!file_exists($filename)) {
-    exec("python3 news.py");
-  }
-
-  $countDown = 10;
-  while(!file_exists($filename) && $countDown >= 0) {
-    sleep(3);
-    $countDown -= 1;
-  }
-
-  if ($countDown < 0) {
-    echo "[error]";
-    exit(1);
-  }
+  // $filename = "news_data.txt";
+  //
+  // if (!file_exists($filename)) {
+  //   exec("python3 news.py");
+  // }
+  //
+  // $countDown = 10;
+  // while(!file_exists($filename) && $countDown >= 0) {
+  //   sleep(3);
+  //   $countDown -= 1;
+  // }
+  //
+  // if ($countDown < 0) {
+  //   echo "[error]";
+  //   exit(1);
+  // }
 
   $myfile = fopen($filename, "r") or die("Unable to open file!");
   echo fread($myfile,filesize($filename));
