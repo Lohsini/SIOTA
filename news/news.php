6d4ba85a6ce4ef7c0a6cee1e4832977e925e16e0
Lohsini <lohi3152@gmail.com>
2020-03-26 11:39:03 +0800

    test

--- news/news.php
+++ news/news.php
@@ -14,26 +14,26 @@ if ($now - $last > 10) {
   fwrite($file,$now);
   fclose($file);
 
-//   $filename = "news_data.txt";
-//
-//   if (!file_exists($filename)) {
-//     exec("python3 news.py");
-//   }
-//
-//   $countDown = 10;
-//   while(!file_exists($filename) && $countDown >= 0) {
-//     sleep(3);
-//     $countDown -= 1;
-//   }
-//
-//   if ($countDown < 0) {
-//     echo "[error]";
-//     exit(1);
-//   }
-//
-//   $myfile = fopen($filename, "r") or die("Unable to open file!");
-//   echo fread($myfile,filesize($filename));
-//   fclose($myfile);
+  $filename = "news_data.txt";
+
+  if (!file_exists($filename)) {
+    exec("python3 news.py");
+  }
+
+  $countDown = 10;
+  while(!file_exists($filename) && $countDown >= 0) {
+    sleep(3);
+    $countDown -= 1;
+  }
+
+  if ($countDown < 0) {
+    echo "[error]";
+    exit(1);
+  }
+
+  $myfile = fopen($filename, "r") or die("Unable to open file!");
+  echo fread($myfile,filesize($filename));
+  fclose($myfile);
 }
 // else {
 //  echo "目前狀態：尚未更新";
