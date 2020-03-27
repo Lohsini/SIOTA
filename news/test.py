e0effb919b16352fc2b863da87dba8b7fd9be850
Lohsini <lohi3152@gmail.com>
2020-03-27 11:35:29 +0800

    requests

--- news/test.py
+++ news/test.py
@@ -5,11 +5,11 @@ import time
 api = "https://www.googleapis.com/customsearch/v1"
 key = "AIzaSyDl7lKKeyRZLseiVKaz6Escr_vtcYTHFhs"
 searchId = "010230242516722102980:n0ssdaoccan"
-query = "劉真 心臟"
+query = "肺炎 確診"
 
 
 res = requests.get(api + "?key=" + key + "&cx=" + searchId + "&q=" + query)
-searchResult = json.loads(res.text)
+# searchResult = json.loads(res.text)
 
 
 f = open("data.txt", "w")
