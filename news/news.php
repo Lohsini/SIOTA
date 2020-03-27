149512ac6b506ff71e3f41f0b88fed5587c48f6f
Lohsini <lohi3152@gmail.com>
2020-03-27 11:26:32 +0800

    move sleep()

--- news/news.php
+++ news/news.php
@@ -6,7 +6,7 @@ header("Access-Control-Allow-Origin: *");
 // $now = time();
 echo "啟動前等10秒".time();
 exec("python3 test.py");
-sleep(10);
+// sleep(10);
 $datafile = "data.txt";
 echo "十秒後".time();
 
