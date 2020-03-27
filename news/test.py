90aa66d733d115862c17adc695e63c8d1147939d
Lohsini <lohi3152@gmail.com>
2020-03-27 11:49:20 +0800

    str(res) only eng

--- news/test.py
+++ news/test.py
@@ -8,12 +8,12 @@ searchId = "010230242516722102980:n0ssdaoccan"
 query = "肺炎 確診"
 #
 #
-res = api + "?key=" + key + "&cx=" + searchId + "&q=" + query
+res = api + "?key=" + key + "&cx=" + searchId + "&q="
 # res = requests.get(api + "?key=" + key + "&cx=" + searchId + "&q=" + query)
 # searchResult = json.loads(res.text)
 
 b=api +"你可以看看"+api+ "&cx="+key
 
 f = open("data.txt", "w")
-f.write(str(b))
+f.write(str(res))
 f.close()
