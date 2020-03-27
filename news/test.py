531cecd8e68f94f24f82438e5a3c45b6d0174d48
Lohsini <lohi3152@gmail.com>
2020-03-27 11:41:20 +0800

    import time

--- news/test.py
+++ news/test.py
@@ -2,17 +2,18 @@
 # import json
 import time
 
-api = "https://www.googleapis.com/customsearch/v1"
-key = "AIzaSyDl7lKKeyRZLseiVKaz6Escr_vtcYTHFhs"
-searchId = "010230242516722102980:n0ssdaoccan"
-query = "肺炎 確診"
-
-
-res = api + "?key=" + key + "&cx=" + searchId + "&q=" + query
+# api = "https://www.googleapis.com/customsearch/v1"
+# key = "AIzaSyDl7lKKeyRZLseiVKaz6Escr_vtcYTHFhs"
+# searchId = "010230242516722102980:n0ssdaoccan"
+# query = "肺炎 確診"
+#
+#
+# res = api + "?key=" + key + "&cx=" + searchId + "&q=" + query
 # res = requests.get(api + "?key=" + key + "&cx=" + searchId + "&q=" + query)
 # searchResult = json.loads(res.text)
 
+b="asdasdasd"
 
 f = open("data.txt", "w")
-f.write(str(res))
+f.write(str(b))
 f.close()
