88d3ca8b4374213d892a2b3940a13bea14383de3
Lohsini <lohi3152@gmail.com>
2020-03-27 11:32:07 +0800

    import json requests

--- news/news.py
+++ news/news.py
@@ -10,7 +10,6 @@ query = "劉真 心臟"
 res = requests.get(api + "?key=" + key + "&cx=" + searchId + "&q=" + query)
 searchResult = json.loads(res.text)
 
-num = 0
 array = []
 class News:
     def __init__(self, title, href):
@@ -21,22 +20,14 @@ class News:
 for n in searchResult["items"]:
     title = n["title"]
     href = n["link"]
-    num = num+1
-    # print("這是第" + str(num) + "則新聞，新聞標題：『" + title + "』，網址：" + link)
     result = News(title, href)
-    # print("這是第" + str(num) + "則新聞，類型是"+result.type +"，新聞標題：『" + result.title + "』，網址：" + result.href)
     array.append(result)
-    # array.append({type: result.type, title: result.title, link: result.link})
-# print(array)
 
 class MyEncoder(json.JSONEncoder):
     def default(self, obj):
         if not isinstance(obj, News):
             return super(MyEncoder, self).default(obj)
-
         return obj.__dict__
-
-# print (json.dumps(array, cls = MyEncoder))
 
 f = open("news_data.txt", "w")
 f.write(json.dumps(array, cls = MyEncoder))
--- news/test.py
+++ news/test.py
@@ -1,7 +1,17 @@
+import requests
+import json
 import time
-a=time.time()
-b="su3cl3ji3g4vup u6"
+
+api = "https://www.googleapis.com/customsearch/v1"
+key = "AIzaSyDl7lKKeyRZLseiVKaz6Escr_vtcYTHFhs"
+searchId = "010230242516722102980:n0ssdaoccan"
+query = "劉真 心臟"
+
+
+res = requests.get(api + "?key=" + key + "&cx=" + searchId + "&q=" + query)
+searchResult = json.loads(res.text)
+
 
 f = open("data.txt", "w")
-f.write(str(b))
+f.write(str(searchResult))
 f.close()
