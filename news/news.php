f9500ca2e2114ba66c1702a40e547803a3bb4560
Lohi <lohi3152@gmail.com>
2020-04-01 10:57:33 +0800

    fix data fwrite

--- news/news.php
+++ news/news.php
@@ -10,30 +10,18 @@ $timestamp = fopen($timestampfile,"r+");
 $last = fread($timestamp,"100");
 fclose($timestamp);
 
-// 下面這一塊換成php的code
-get_news()
-//
-// $data = fopen($datafile, "r") or die("Unable to open file!");
-// echo fread($data,filesize($datafile));
-// fclose($data);
+$data = fopen($datafile, "r") or die("Unable to open file!");
+echo fread($data,filesize($datafile));
+fclose($data);
 
-// 你可以寫成一個function, 比較彈性
-// 呼叫google api, 拿回result <--要上網查
-
-// 將result轉成json <--上網查
-
-// 剩下的就是要用爬蟲來讀result_json, 取出你要的資訊, 存到你自己的class <- 上網查
-
-//
 // if ($now - $last > 60) {
-//   exec("php parse_news.php");
-//   echo "已啟用parse_news.php//";
+//   get_news()
+//   // echo "已啟用parse_news";
 //
 //   $timestamp = fopen($timestampfile,"w+");
 //   fwrite($timestamp,$now);
 //   fclose($timestamp);
-//   echo "新檔案ok時間標已經換好了//";
-//
+//   // echo "新檔案ok時間標已經換好了";
 
 //   if (!file_exists($datafile)) {
 //     echo "如果時間超過且檔案不在，就執行原有的//";
@@ -55,6 +43,8 @@ get_news()
 // }
 
 // else {
-//   exec("php parse_news.php");
+//   $data = fopen($datafile, "r") or die("Unable to open file!");
+//   echo fread($data,filesize($datafile));
+//   fclose($data);
 // }
 ?>
